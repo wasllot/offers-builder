b9dc89a54c6441105134ccd74019afa96abccbf7
Reinaldo <rei.vzl@gmail.com>
2020-10-08 12:40:24 -0400

    first commit

---
+++ packages/Webkul/Velocity/src/Resources/lang/es/app.php
@@ -0,0 +1,297 @@
+<?php
+
+return [
+    'admin'         => [
+        'system'    => [
+            'velocity' => [
+                'general'  => 'General',
+                'category'  => 'Categoría',
+                'settings'  => 'Ajustes',
+                'extension_name' => 'Tema',
+                'error-module-inactive' => 'Precaución: El tema está inactivo.',
+            ],
+
+            'settings' => [
+                'channels'=> [
+                    'subscription_bar' => 'Centenido de la barra de suscripción'
+                ],
+            ],
+
+            'general' => [
+                'status' => 'Estado',
+                'active' => 'Activo',
+                'inactive' => 'Inactivo',
+            ],
+            'category' => [
+                'all' => 'Todas',
+                'left' => 'Izquierda',
+                'right' => 'Derecha',
+                'active' => 'Activo',
+                'custom' => 'Personalizado',
+                'inactive' => 'Inactivo',
+                'image-alignment' => 'Alineamiento de imagen',
+                'icon-status' => 'Ícono del estado de la categoría',
+                'image-status' => 'Imagen de la categoría',
+                'sub-category-show' => 'Mostrar subcategoría',
+                'image-height' => 'Altura de la imagen [en píxeles]',
+                'image-width' => 'Ancho de la imagen [en Píxeles]',
+                'show-tooltip' => 'Mostrar tooltip de la categoría',
+                'num-sub-category' => 'Número de subcategorías',
+            ]
+        ],
+        'layouts'   => [
+            'velocity' => 'Tema',
+            'cms-pages' => 'CMS',
+            'meta-data' => 'Datos Meta',
+            'category-menu' => 'Menú de Categorías',
+            'header-content' => 'Contenido de la cabecera',
+        ],
+        'contents'  => [
+            'self' => 'Yo',
+            'active' => 'Activo',
+            'new-tab' => 'Nueva tabla',
+            'inactive' => 'Inactivo',
+            'title' => 'Lista de contenido',
+            'select' => '-- Seleccionar --',
+            'add-title' => 'Agregar contenido',
+            'btn-add-content' => 'Agregar contenido',
+            'save-btn-title' => 'Guardar Contenido',
+            'autocomplete' => '[Autocompletar]',
+            'no-result-found' => 'Sin resultados.',
+            'search-hint' => 'Busco un producto aquí...',
+            'mass-delete-success' => 'Contenido seleccionado eliminado correctamente.',
+            'tab' => [
+                'page' => 'Ajustes de la página',
+                'content' => 'Ajustes de contenido',
+                'meta_content' => 'Datos meta',
+            ],
+            'page' => [
+                'title' => 'Titulo',
+                'status' => 'Estado',
+                'position' => 'Posición',
+            ],
+            'content' => [
+                'content-type' => 'Tipo de contenido',
+                'custom-title' => 'Título personalizado',
+                'category-slug' => 'Slug de la categoría',
+                'link-target' => 'Target del link de la página',
+                'custom-product' => 'Almacenar productos',
+                'custom-heading' => 'Título personalizado',
+                'catalog-type' => 'Tipo de catálogo de productos',
+                'static-description' => 'Descripción del contenido',
+                'page-link' => 'Link de la página [e.g. http://example.com/../../]',
+            ],
+            'datagrid' => [
+                'id' => 'Id',
+                'title' => 'Título',
+                'status' => 'Estado',
+                'position' => 'Posición',
+                'content-type' => 'Tipo de contenido',
+            ]
+        ],
+        'meta-data' => [
+            'footer' => 'Pie de página',
+            'title' => 'Meta datos del tema',
+            'activate-slider' => 'Activar Slider',
+            'home-page-content' => 'Contendo del inicio',
+            'footer-left-content' => 'Contenido de la izquierda del pie de página',
+            'subscription-content' => 'Contenido de la barra de suscripción',
+            'sidebar-categories' => 'Sidebar Categories',
+            'footer-left-raw-content' => '<p>Importamos todo tipo de productos desde USA y CHINA. Además, realizamos envíos a lo largo del país y al extranjero. </p>',
+            'slider-path' => 'Ruta del Slider',
+            'category-logo' => 'Ícono de la categoría',
+            'product-policy' => 'Política del producto',
+            'update-meta-data' => 'Actualizar meta datos',
+            'product-view-image' => 'Imagen del producto',
+            'advertisement-two' => 'Banner de 4 imágenes',
+            'advertisement-one' => 'Banner de una imagen',
+            'footer-middle-content' => 'Contenido del centro del pie de página',
+            'advertisement-four' => 'Banner de 4 imágenes',
+            'advertisement-three' => 'Banner de 3 imágenes',
+            'images' => 'Imágenes',
+            'general' => 'General',
+            'add-image-btn-title' => 'Añadir imagen'
+        ],
+        'category'  => [
+            'save-btn-title' => 'Guardar menu',
+            'title' => 'Lista de menú de categoría',
+            'add-title' => 'Agregar menú de contenido',
+            'edit-title' => 'Editar menú de contenido',
+            'btn-add-category' => 'Agregar contendo de la categoría',
+            'datagrid' => [
+                'category-id' => 'Id de Categoría',
+                'category-name' => 'Nombre de la categorÍa',
+                'category-icon' => 'Ícono de la categoría',
+                'category-status' => 'Estado',
+            ],
+            'tab' => [
+                'general' => 'General',
+            ],
+            'status' => 'Estado',
+            'active' => 'Activo',
+            'inactive' => 'Inactivo',
+            'select' => '-- Seleccionar --',
+            'icon-class' => 'Clase de ícono',
+            'select-category' => 'Escoja una categoria',
+            'tooltip-content' => 'Tooltip de contenido',
+            'mass-delete-success' => 'Menú de categoría seleccionado eliminado correctamente',
+        ],
+        'general'   => [
+            'locale_logo' => 'Logo local',
+        ],
+    ],
+
+    'home'          => [
+        'view-all'           => 'Ver todo',
+        'add-to-cart'        => 'Agregar al carrrito',
+        'hot-categories'     => 'Categorías recurrentes',
+        'payment-methods'    => 'Métodos de pago',
+        'customer-reviews'   => 'Comentarios de los clientes',
+        'shipping-methods'   => 'Métodos de envío',
+        'popular-categories' => 'Categorías populares',
+    ],
+
+    'header'        => [
+        'cart'              => 'Carro',
+        'cart'              => 'Carro',
+        'guest'             => 'Invitado',
+        'logout'            => 'Salir',
+        'title'             => 'Cuenta',
+        'account'           => 'Cuenta',
+        'profile'           => 'Perfil',
+        'wishlist'          => 'Lista de deseos',
+        'all-categories'    => 'Todas las categorí<s',
+        'search-text'       => 'Busca tus productos aquí',
+        'welcome-message'   => 'Bienvenido, :customer_name',
+        'dropdown-text'     => 'Gestionar carrito, órdenes & Lista de deseos',
+    ],
+
+    'menu-navbar'   => [
+        'text-more'     => 'Más',
+        'text-category' => 'Comprar por categoría',
+    ],
+
+    'minicart'      => [
+        'cart'      => 'Carrito',
+        'view-cart' => 'Ver carrito',
+    ],
+
+    'checkout'      => [
+        'qty'       => 'Cantidad',
+        'checkout'  => 'Revisión',
+        'cart'      => [
+            'view-cart'     => 'Ver carrito',
+            'cart-summary'  => 'Resumen de la compra',
+        ],
+        'qty'       => 'Cantidad',
+        'items'     => 'Artículos',
+        'subtotal'  => 'Subtotal',
+        'sub-total' => 'Sub Total',
+        'proceed'   => 'Pasar a la pagar',
+    ],
+
+    'customer'      => [
+        'compare'           => [
+            'text'                  => 'Comparar',
+            'compare_similar_items' => 'Comparar artículos similares',
+            'added'                 => 'Artículo añadido a la listad de comparación',
+            'already_added'         => 'El artículo ya se encuentra en la lista',
+            'removed'               => 'Artículo eliminado de la listad de comparación',
+            'empty-text'            => "No tienes ningún artículo en la listad de comparación",
+            'product_image'         => 'Imagen del producto',
+            'actions'               => 'Acciones',
+        ],
+        'login-form'        => [
+            'sign-up'               => 'Registrarse',
+            'new-customer'          => 'Nuevo cliente',
+            'customer-login'        => 'Ingresar',
+            'registered-user'       => 'Usuario registrado',
+            'your-email-address'    => 'Tu correo electrónico',
+            'form-login-text'       => 'Si ya tienes una cuenta, ingresa con tu correo',
+        ],
+        'signup-form'       => [
+            'login'             => 'Ingresar',
+            'become-user'       => 'Comviértete en usuario',
+            'user-registration' => 'Registro de usuario',
+            'form-sginup-text'  => 'Si te registras en nuestra tienda, estaremos agradecidos de que seas parte de nosotros.',
+        ],
+        'forget-password'   => [
+            'login'                 => 'Ingresar',
+            'forgot-password'       => 'Olvidé mi contraseña',
+            'recover-password'      => 'Recuperar contraseña',
+            'recover-password-text' => 'Si olvidaste tu contraseña, recupérala ingresando tu correo.',
+        ]
+    ],
+
+    'error'         => [
+        'go-to-home'            => 'Ir a inicio',
+        'page-lost-short'       => 'Contenido perdido de la página',
+        'something_went_wrong'  => 'Algo mal ocurrió',
+        'page-lost-description' => "La página que estás buscando no se encuentra disponble. Intenta buscar otra vez o utiliza el botón de abajo para volver.",
+    ],
+
+    'products'      => [
+        'text'              => 'Productos',
+        'details'           => 'Detailles',
+        'reviews-title'     => 'Comentarios',
+        'reviewed'          => 'Comentado',
+        'review-by'         => 'Comentado pos',
+        'quick-view'        => 'Vista rápida',
+        'not-available'     => 'No disponible',
+        'submit-review'     => 'Enviar comentario',
+        'ratings'           => ':totalRatings Calificaciones',
+        'reviews-count'     => ':totalReviews Comentarios',
+        'customer-rating'   => 'Calificación del cliente',
+        'more-infomation'   => 'Más información',
+        'view-all-reviews'  => 'Ver todos los comentarios',
+        'write-your-review' => 'Escribe tu comentario',
+        'short-description' => 'Descripción corta',
+        'recently-viewed'   => 'Productos vistos recientemente',
+        'be-first-review'   => 'Se el primero en escribir un comentario',
+    ],
+
+    'shop'          => [
+        'gender'    => [
+            'male'   => 'Hombre',
+            'other'  => 'Otro',
+            'female' => 'Mujer',
+        ],
+        'general'   => [
+            'no'                     => 'No',
+            'yes'                    => 'Sí',
+            'view'                   => 'Ver',
+            'filter'                 => 'Filtro',
+            'orders'                 => 'Órdenes',
+            'update'                 => 'Actualizar',
+            'reviews'                => 'Comentarios',
+            'currencies'             => 'Monedas',
+            'addresses'              => 'Direcciones',
+            'top-brands'             => 'Principales Marcas',
+            'new-password'           => 'Nueva contraseña',
+            'downloadables'          => 'Productos descargables',
+            'confirm-new-password'   => 'Confirmar nueva contraseña',
+            'enter-current-password' => 'Ingresa tu actual contraseña',
+
+            'alert' => [
+                'info'      => 'Info',
+                'error'     => 'Error',
+                'success'   => 'Bien',
+                'warning'   => 'Alerta',
+            ],
+        ],
+        'wishlist'  => [
+            'add-wishlist-text'     => 'Agregar producto a la lista de deseos',
+            'remove-wishlist-text'  => 'Eliminar producto de la lista de deseos'
+        ]
+    ],
+
+    'responsive'    => [
+        'header' => [
+            'done'      => 'Hecho',
+            'languages' => 'Lenguajes',
+            'greeting'  => 'Bienvenido, :customer !',
+        ]
+    ],
+]
+
+?>
---
+++ resources/assets/js/components/SearchBarComponent.vue
@@ -0,0 +1,71 @@
+<template>
+				              	<form method="GET" role="search" id="search-form" :action="route" class="search-job">
+				              		<div class="row no-gutters">
+				              			<div class="col-md mr-md-2">
+				              				<div class="form-group">
+					              				<div class="form-field">
+					              					<div class="icon"><span class="icon-briefcase"></span></div>
+									                <input type="text" class="form-control" placeholder="eg. Garphic. Web Developer">
+									              </div>
+								              </div>
+				              			</div>
+				              			<div class="col-md mr-md-2">
+				              				<div class="form-group">
+				              					<div class="form-field">
+					              					<div class="select-wrap">
+							                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+							                      <select name="" id="" class="form-control">
+							                      	<option  v-for="(category, index) in $root.sharedRootCategories" :key="index" :value="category.id">{{category.name}}</option>
+							                      </select>
+							                    </div>
+									              </div>
+								              </div>
+				              			</div>
+				              			<div class="col-md mr-md-2">
+				              				<div class="form-group">
+				              					<div class="form-field">
+					              					<div class="icon"><span class="icon-map-marker"></span></div>
+									                <input type="text" class="form-control" placeholder="Location">
+									              </div>
+								              </div>
+				              			</div>
+				              			<div class="col-md">
+				              				<div class="form-group">
+				              					<div class="form-field">
+								                	<button type="submit" class="form-control btn btn-primary">Search</button>
+									              </div>
+								              </div>
+				              			</div>
+				              		</div>
+				              	</form>
+</template>
+
+<script>
+    export default {
+        props: ['route', 'iscustomer'],
+        data(){
+            return{
+                compareCount: 0,
+                wishlistCount: 0,
+                searchedQuery: [],
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        created: function () {
+            let searchedItem = window.location.search.replace("?", "");
+            searchedItem = searchedItem.split('&');
+
+            let updatedSearchedCollection = {};
+
+            searchedItem.forEach(item => {
+                let splitedItem = item.split('=');
+                updatedSearchedCollection[splitedItem[0]] = splitedItem[1];
+            });
+
+            this.searchedQuery = updatedSearchedCollection;
+        },
+
+    }
+</script>
