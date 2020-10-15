98092a60da25089931b7defeb9c00fa368010ef8
Reinaldo <rei.vzl@gmail.com>
2020-10-15 17:40:05 -0400

    database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => '3.131.154.215',
-            'port' => '3306',
-            'database' => 'jobacaso_offers',
-            'username' => 'jobacaso_1',
-            'password' => 'jobacaso$$$',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
