656012783463958a3a4b40bdbc1da1a50bea1f23
Reinaldo <rei.vzl@gmail.com>
2020-10-15 23:02:31 -0400

    https schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if(env('APP_ENV') !== 'local'){
+            
+            URL::forceScheme('https');
+
+        }
     }
 
     /**
