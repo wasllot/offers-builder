f201c98d8ad667a154d888d6613141b0736ab0a1
Wasllot dev <38118800+wasllot@users.noreply.github.com>
2020-10-16 10:14:22 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'remotemysql.com',
+            'port' => '3306',
+            'database' => 'WV1tbmL52o',
+            'username' => 'WV1tbmL52o',
+            'password' => 'KYH5YHpoBF',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
